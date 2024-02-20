a3219382e61f4f068bd01c02f27ab4d319a6282d
kirillkeril <89710374+kirillkeril@users.noreply.github.com>
2024-02-20 20:36:26 +0500

    login and register fix

--- lib/app/modules/auth/controllers/auth_controller.dart
+++ lib/app/modules/auth/controllers/auth_controller.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:strihkod/app/data/auth_service_dart_provider.dart';
 
 class AuthController extends GetxController {
   TextEditingController emailController = TextEditingController();
   TextEditingController passController = TextEditingController();
+  final service = AuthServiceDartProvider();
 
   final _isLogin = true.obs;
 
   String get loginText => _isLogin.value ? "Вход" : "Регистрация";
-  String get alternativeText => _isLogin.value ? "Нет аккаунта?" : "Уже есть аккаунт?";
+  String get alternativeText =>
+      _isLogin.value ? "Нет аккаунта?" : "Уже есть аккаунт?";
 
-  Future<void> auth() async {} // TODO
+  Future<void> auth() async {
+    if (_isLogin == true) {
+      var res = await service.signIn(emailController.text, passController.text);
+      print(res);
+    } else {
+      var res = await service.signUp(emailController.text, passController.text);
+      print(res);
+    }
+  } // TODO
 
   Future<void> changeAuthType() async => _isLogin.value = !_isLogin.value;
 }
